0af965dbb63f5f45f22e8c939cf62b5c1321c608
AnneCride <acpetit@icloud.com>
2017-10-26 17:10:57 +0200

    contact form routes

--- myapp/routes/admin.js
+++ myapp/routes/admin.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
+const multer = require('multer');
+const fs = require('fs');
+const upload = multer({ det: 'tmp/'});
 
 
- const connection = mysql.createConnection({
+var connection = mysql.createConnection({
  	host     : 'localhost',
 	user     : 'root',
 	password : 'root',
 	database : 'mydb'
 });
 
+connection.connect();
 
- router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	// Hello session !
 	// res.send(req.session.connect);
-	// Si la personne est connectée on affiche la page 
+	// Si la personne est connectée on affiche la page
 	// Si la personne n'est pas connectée on le redirige sur la page de connexion
   if(req.session.connect) {
   	res.render('admin');
@@ -22,23 +26,28 @@ const mysql = require('mysql');
   	res.redirect("/login");
   }
 });
- 
- 
-/* router.get('/', function(req, res, next) {
+
+ //GET home page
+router.get('/', function(req, res, next) {
 	// Page de connexion
 
-	connection.query(`select * from users where name= ?`,[req.body.toto], function (error, results, fields) {
-	 	 res.render('admin', { 
+	/*connection.query(`select * from users where name= ?',[req.body.toto], function (error, results, fields) {
+	 	 res.render('admin', {
 	 	 	title: 'Express',
 	 	 	error : JSON.stringify(error),
-	 	 	results:  JSON.stringify(results), 
+	 	 	results:  JSON.stringify(results),
 	 	 	fields : JSON.stringify(fields)
 	 	 	 });
- 	 
+
 	});
 
-  
-}); */
+
+});
+*/
+
+
+
+
 
 router.post('/', function(req, res, next) {
 	// Ici on gère les informations de l'utilisateur
@@ -49,70 +58,52 @@ router.post('/', function(req, res, next) {
 	// Tester si l'utilisateur existe en BDD  -> Comparer le nom (login) / le password
 	let login= req.body.username;
 	let password = req.body.password ;
-	// select name, password from users where name='${var}' and password='wild';
-	//` text ${var} fzoeijfzeoj ${var2}` 
 
-	 	connection.query(`select * from user where pseudo= ? and password= ?` ,[login, password], function (error, results, fields) {
+	/*connection.query('select * from users where name= "${login}"
+		and password="${password}"', function (error, results, fields) {
  	 if (results.length==0) {
  	 	res.send("Erreur");
  	 }else{
  	 	req.session.connect=true;
  	 	res.redirect("/admin");
 
+	}
+
+	});
+
+	if (login == users[0].name && password == users[0].mdp) {
+ 	 	req.session.connect=true;
+ 	 	res.redirect("/admin");
+ 	 }else{
+ 	 	res.send("Erreur");
+
  	 }
- 	 
-	}); 
 
 
-	// Si faux on lui envoie un message pour l'informer 
-	// Si vrai -> On ouvre la session & on le redirige sur /admin 
+	// Si faux on lui envoie un message pour l'informer
+	// Si vrai -> On ouvre la session & on le redirige sur /admin
 
-  	 
+
 });
 
 
 
-
-
-
-
-/* .router.post('/', function(req, res, next) {
-	// Ici on gère les informations de l'utilisateur
-
-	//res.send(req.body.username);
-	//res.send(req.body['username']);
-
-	// Tester si l'utilisateur existe en BDD  -> Comparer le nom (login) / le password
-	let login= req.body.username;
-	let password = req.body.password ;
-	// select name, password from users where name='${var}' and password='wild';
-	//` text ${var} fzoeijfzeoj ${var2}` 
-
-	 	connection.query(`select * from user where pseudo= "${login}" 
-		and password="${password}"`, function (error, results, fields) {
- 	 if (results.length==0) {
- 	 	res.send("Erreur");
- 	 }else{
- 	 	req.session.connect=true;
- 	 	res.redirect("/admin");
-
- 	 }
- 	 
-	}); 
-
-
-	// Si faux on lui envoie un message pour l'informer 
-	// Si vrai -> On ouvre la session & on le redirige sur /admin 
-
-  	 
-}); */
-
-
-
-router.get('/logout', function(req, res, next) {
+router.get("/logout", function(req, res, next) {
   req.session.connect = false;
   res.redirect("/login");
-  
+
+});
+
+router.get('/produit-:productID(\\d+)', function(req, res, next) {
+  connection.query('select * from products where id = ?' , [req.params.productID], function (error, results, fields);
+  res.render('produit', {
+		informations: products[req.params.productID]
+	});
+
+router.get('/produits', function(req,req,next) {
+	res.render('produit',{
+		informations: products
+	});
 });
 
 module.exports = router;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -17,9 +17,15 @@ router.get('/presse', function(req, res, next) {
   res.render('presse',{menu_index: 3});
 });
 
+//route formulaire de contact
 router.get('/contact', function(req, res, next) {
   //res.sendFile(__dirname+'/public/realisations.html');
   res.render('contact',{menu_index: 4});
+});
+
+router.post('/contact', function(req, res, next) {
+  //res.sendFile(__dirname+'/public/realisations.html');
+  res.render('contact-confirm',{menu_index: 4});
 });
 
 router.get('/login', function(req, res, next) {
@@ -27,16 +33,26 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
+router.get('/produit-id:id(\\d+)', function(req, res, next) {
+  //res.sendFile(__dirname+'/public/realisations.html');
+  connection.query('select * from products',function(error, results){
 
+  res.render('produit');{ informations: products}
+});
+});
+
+router.get('/admin/edit-produits', function(req, res, next) {
+res.render('edit-produits');
+});
+
+router.post('/admin/edit-produits', function(req, res, next) {
+  // insert into produit values ( req.body.titre, req.body.description)
+});
 
 router.get('/test', function(req, res, next) {
   //res.sendFile(__dirname+'/public/realisations.html');
   res.render('test',{presse: true});
 });
-
-
-
-
 
 
 module.exports = router;
