465ce1b9b36a655e09a3fcbb970ed639666baf3c
AnneCride <acpetit@icloud.com>
2017-11-01 21:12:05 +0100

    editer article et favicon

--- myapp/routes/admin.js
+++ myapp/routes/admin.js
@@ -24,63 +24,90 @@ router.get("/logout", function(req, res, next) {
 });
 
 
-router.get('/edit-article', function(req, res, next) {
-  // res.sendFile(__dirname+'/public/index1.html');
-  res.render('edit-article');
-});
 
-
-
-
-
-// GET /admin/
-router.get('/', function(req, res, next) {	
-	connection.query('SELECT DISTINCT article.idarticle, article.*, media.sourceName FROM article, media, category, article_has_category WHERE idarticle=article_has_category.article_idarticle AND article_has_category.category_idcategory=idcategory AND idarticle=media.article_idarticle ;', function (error, results, fields) {
+// GET /admin/   Afficher la liste des produits de la table 'products'
+router.get('/', function(req, res, next) {
+connection.query(`SELECT distinct (a.idarticle), a.title, a.text, c.name, m.*
+FROM article a
+JOIN article_has_category ac
+ON a.idarticle = ac.article_idarticle
+JOIN category c
+ON ac.category_idcategory = c.idcategory
+LEFT JOIN media m
+ON a.idarticle = m.article_idarticle;`, function (error, results, fields) {
 	  if (error) throw error;
 	  //console.log(results)
-		res.render('admin-index', {products:results});
+		res.render('admin-index', {
+      product:results
+    });
 	  //console.log(results);
 	});
-	// Afficher la liste des produits de la table 'products' 
-	//;
 });
 
-// GET /admin/create-product 
+
+
+
+
+// GET /admin/create-product
 router.get('/create-product', function(req, res, next) {
 	res.render('admin-create');
 });
 
-// POST /admin/create-product 
+// POST /admin/create-product
 router.post('/create-product', upload.single('product_sourceName'), function(req, res, next) {
 	// Ajouter un produit dans la table 'products'
-	console.log(req.file);
+	//console.log(req.file);
 	if (req.file.size < (3*1024*1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpg' || req.file.mimetype == 'image/jpeg') ) {
 		fs.rename(req.file.path,'public/img/'+req.file.originalname);
-		
+
 	} else {
 		res.send('Vous avez fait une erreur dans le téléchargement')
 	}
-
 	connection.query('insert into article values(null, 1, ?, ?, NOW());',[req.body.title,req.body.text], function (error, results, fields) {
 	  if (error) throw error;
 		res.redirect('/admin');
 	  //console.log(results);
 	});
-
-	
-	console.log(req.body);
+	//console.log(req.body);
 
 });
 
-// req.params -> /monlien-:id 
-// req.body -> POST 
+//GET/admin/  edition article
+router.get('/edit-product/:idarticle(\\d+)', function(req, res) {
+  //console.log('editer un produit');
+  connection.query('select * from article where idarticle=?', [req.params.idarticle], function(error, results) {
+    if (error) throw error;
+      res.render('admin-edit', {
+        product:results[0]
+      });
+  })
+
+
+});
+
+router.post('/edit-product/:idarticle(\\d+)', function(req, res, next) {
+  connection.query('update article set title = ?, text = ? where idarticle=?',[req.body.title, req.body.text, req.bod.idarticle], function(error, results, fields) {
+console.log(results);
+   if (error) throw error;
+   res.redirect('/admin');
+  })
+
+  // res.sendFile(__dirname+'/public/index1.html');
+
+
+
+});
+
+
+// req.params -> /monlien-:id
+// req.body -> POST
 // req.query -> monlien?id=1
 
-// GET /admin/delete-product 
-router.get('/delete-product', function(req, res, next) {
-	// Supprimer le produit en recupérant l'id dans la query 
-	//delete from products where id=
-	connection.query('delete from article where idarticle=?',[req.query.id],function (error, results, fields) {
+// GET /admin/delete-product
+router.get('/delete-product/:idarticle(\\d+)', function(req, res, next) {
+	// Supprimer le produit en recupérant l'id dans la query
+console.log('delete');
+	connection.query('delete from article where idarticle=?',[req.query.idarticle],function (error, results, fields) {
 	  if (error) throw error;
 		res.redirect('/admin');
 	  //console.log(results);
