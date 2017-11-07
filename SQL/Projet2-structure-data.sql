7211e3f0858a4e7c0908210da5318fcd96935ce3
plesiosaure <42@e-xode.com>
2017-11-07 22:26:10 +0100

    sql populate

---
+++ SQL/Projet2-structure-data.sql
@@ -0,0 +1,192 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	5.1.73-community
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `idarticle` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `text` text,
+  `lastUpdate` datetime NOT NULL,
+  PRIMARY KEY (`idarticle`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (1,'Appartement 503 m2','Cet appartement a été entièrement rénové','2017-10-26 16:26:36'),(2,'Appartement 50 m2','Cet appartement a été entièrement rénové','2017-10-26 16:26:50'),(3,'Appartement 50 m2','Cet appartement a été entièrement rénové','2017-10-26 16:26:53'),(4,'Appartement 50 m2','Cet appartement a été entièrement rénové','2017-10-26 16:26:54'),(5,'Appartement 50 m2','Cet appartement a été entièrement rénové','2017-10-26 16:26:59'),(6,'Appartement 50 m2','Cet appartement a été entièrement rénové','2017-10-26 16:27:00'),(7,'Le Figaro Madame','Rubrique immobilier','2017-10-26 16:27:01'),(8,'Le Figaro Madame','Rubrique immobilier','2017-10-26 16:27:03'),(9,'Le Figaro Madame','Rubrique immobilier','2017-10-26 16:27:04'),(13,'APPARTEMENT DUPLEX DE 90M2, PARIS 2ÈME','Situé au 4ème et 5ème étage, ce beau duplex est mis en valeur par sa luminosité. En contraste aux murs blancs, le parquet hausmannien en pointe de Hongrie a été repeint en noir, ce qui lui apporte une touche moderne. Dans le salon / cuisine ouverte, la grande bibliothèque d’angle a été dessinée et conçue sur mesure en bois de palissandre, et sert également de bureau.','2017-11-07 22:01:15'),(14,'APPARTEMENT DE 130M2, PARC MONCEAU PARIS 8ÈME','Dans un immeuble années 70, cet appartement de 130m2 avait, la première fois que je l’ai visité, de la moquette blanche au sol. Entièrement repensé par l’architecte Aurélie Barbey, l’aménagement a donc été revu pour accueillir une famille de quatre personnes.\r\n\r\nAfin d’apporter un côté hausmannien moderne, j’ai choisi un parquet en chêne Pointe de Hongrie mais aspect brut et naturel avec un vernis mat. Le salon, lumineux, donne sur le parc Monceau et donc très agréable avec ses grandes baies vitrées. J’ai dessiné un meuble TV aux couleurs pastels, épuré, qui répond au bleu doux du canapé. Des touches de marbre apportent aussi un côté chic au salon avec la table basse et la table à manger, prolongé par le plan de travail de la cuisine.','2017-11-07 22:03:39'),(15,'LE BARLONE, BAR À COCKTAILS, PARIS 9ÈME','Situé en face du siège historique de la BNP, rue Bergère, le Barlone n’était pas un bar, mais une agence de voyages, complètement transformée pour l’occasion. J’ai souhaité apporter à cet endroit une ambiance chaleureuse, intimiste, dans les tons bleu /vert, apportés par la grande fresque panoramique sur le thème de la nature et les plantes vertes.','2017-11-07 22:04:52'),(16,'APPARTEMENT DE 40M2, PARIS 17ÈME','Situé au 5ème étage, cet appartement très lumineux a été conçu pour un jeune trentenaire dynamique. La décoration a été pensé dans un esprit confortable, épuré avec quelques touches de mobilier vintage.','2017-11-07 22:05:52'),(17,'LOFT 200M2 RUE DE MONT LOUIS, PARIS','C’est dans une ancienne imprimerie que ce loft a été rénové dans les années 2000. La propriétaire, qui habite actuellement à Londres, m’a demandé de refaire la décoration qui n’était plus très actuelle. J’ai donc choisi de mêler ses origines orientales avec une touche de modernité, en essayant de préserver l’esprit industriel du lieu. Un joli rez-de-jardin verdoyant laisse entrevoir le rez-de-chaussée, où deux espaces salon se côtoient avec également un accès à la cuisine – salle à manger. A l’étage, une suite parentale et une chambre d’amis, décorées dans un style classique.','2017-11-07 22:06:53'),(18,'SALON RUE DE LA TOUR D’AUVERGNE, PARIS','Pour ce salon/cuisine au charme ancien du 9ème arrondissement de Paris, j’ai choisi de garder ces jolis murs de pierre et poutres apparentes et de les remettre en valeur en lissant la pierre et appliquer un vernis sur les poutres. J’ai respecté la volonté de ma cliente en intégrant un mobilier vintage, des couleurs douces et surtout un bureau et des étagères sur mesure. Nous avons rénovés la pierre apparente et construit autour du conduit de cheminée des étagères en bois et un bureau pratique avec des rangements.','2017-11-07 22:08:11'),(19,'CHAMBRE DE BÉBÉ, VILLEJUIF','Cette réalisation s’est focalisée sur la chambre, prévue pour être douce puisqu’elle doit accueillir un bébé… L’ambiance est celle d’un cocon, les tons adoptés sont un choix de blancs et de gris. C’est une décoration classique mais qui trouve son originalité dans les quelques touches rétro (papier peint losange, fauteuil rotin…). Afin de respecter la douceur demandée, les matériaux utilisés sont le bois associé au bambou. En outre, la couleur n’a pas été négligée puisqu’on retrouve quelques pointes de gris et de rosé.','2017-11-07 22:08:57'),(20,'APPARTEMENT RUE DARWIN, PARIS','Situé en plein coeur de Montmartre, j’ai rénové ce petit studio de 25m2 destiné à la location saisonnière pour en faire un endroit cosy, agréable et coloré pour les touristes. La demande de ma cliente était d’apporter une ambiance scandinave et lumineuse : grâce à un mur de couleur jaune moutarde, le salon est chaleureux et l’on retrouve cette couleur dans les rideaux en lin et le papier peint fifties. Le bois et le blanc participent à cette atmosphère, ainsi que la décoration murale qui apporte une touche vintage.','2017-11-07 22:09:54'),(21,'APPARTEMENT RUE CLAUZEL, PARIS','Dans le 9ème arrondissement de Paris, à proximité de la rue des Martyrs, cet appartement atypique de 100 m2 au rez-de-chaussée est composé d’une petite terrasse, d’un salon et cuisine et de chambres en souplex. J’ai redécoré cet appartement au style industriel dans des tons chauds : murs gris clairs, bibliothèque sur mesure pour le salon, objets vintage chinés, et tonalités de bois et noirs pour donner une nouvelle vie à la cuisine.','2017-11-07 22:11:42'),(22,'LA MAISON FRANCE 5','«Pour rajeunir et dynamiser son intérieur, Nicolas a fait appel à Marion Alberge, décoratrice d’intérieur. (…) Si confier la décoration de son appartement à une tierce personne n’est pas forcément facile, ce fut très concluant pour Nicolas.»','2017-11-07 22:14:44'),(23,'20 MINUTES','«Miser sur les couleurs chaudes : voici la première des astuces de Marion Alberge pour faire monter la température de son intérieur.»','2017-11-07 22:15:45'),(24,'ELLE A TABLE (JAPON)','«Marion Alberge, décoratrice et styliste pour la presse déco, fait des merveilles dans les intérieurs XXS.»','2017-11-07 22:17:40');
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `article_has_category`
+--
+
+DROP TABLE IF EXISTS `article_has_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article_has_category` (
+  `article_idarticle` int(11) NOT NULL,
+  `category_idcategory` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`article_idarticle`,`category_idcategory`),
+  KEY `fk_article_has_category_category1_idx` (`category_idcategory`),
+  KEY `fk_article_has_category_article1_idx` (`article_idarticle`),
+  CONSTRAINT `fk_article_has_category_article1` FOREIGN KEY (`article_idarticle`) REFERENCES `article` (`idarticle`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_article_has_category_category1` FOREIGN KEY (`category_idcategory`) REFERENCES `category` (`idcategory`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_has_category`
+--
+
+LOCK TABLES `article_has_category` WRITE;
+/*!40000 ALTER TABLE `article_has_category` DISABLE KEYS */;
+INSERT INTO `article_has_category` VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(13,1),(14,1),(15,1),(16,1),(17,1),(18,1),(19,1),(20,1),(21,1),(7,2),(8,2),(9,2),(22,2),(23,2),(24,2);
+/*!40000 ALTER TABLE `article_has_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `idcategory` tinyint(3) unsigned NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcategory`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Réalisations'),(2,'Articles de presse');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `key`
+--
+
+DROP TABLE IF EXISTS `key`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `key` (
+  `idkey` varchar(255) NOT NULL,
+  `expirationDate` datetime NOT NULL,
+  `user_iduser` int(11) NOT NULL,
+  PRIMARY KEY (`idkey`),
+  KEY `fk_key_user_idx` (`user_iduser`),
+  CONSTRAINT `fk_key_user` FOREIGN KEY (`user_iduser`) REFERENCES `user` (`iduser`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `key`
+--
+
+LOCK TABLES `key` WRITE;
+/*!40000 ALTER TABLE `key` DISABLE KEYS */;
+/*!40000 ALTER TABLE `key` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media` (
+  `idmedia` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `legend` varchar(100) DEFAULT NULL,
+  `thumbnailName` varchar(45) NOT NULL,
+  `sourceName` varchar(45) DEFAULT NULL,
+  `article_idarticle` int(11) NOT NULL,
+  `featured` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`idmedia`),
+  KEY `fk_media_article1_idx` (`article_idarticle`),
+  CONSTRAINT `fk_media_article1` FOREIGN KEY (`article_idarticle`) REFERENCES `article` (`idarticle`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media`
+--
+
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+INSERT INTO `media` VALUES (1,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',1,0),(2,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',1,0),(3,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',1,0),(4,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',2,0),(5,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',2,0),(6,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',3,0),(7,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',3,0),(8,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',4,0),(9,'img','','légeCeci est une imagende','vign_real_img2.jpg','real_img2.jpg',4,0),(10,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',4,0),(11,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',5,0),(12,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',5,0),(13,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',6,0),(14,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',6,0),(15,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',6,0),(16,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',1,0),(17,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',2,0),(18,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',3,0),(19,'img','','Ceci est une image','vign_real_img2.jpg','real_img2.jpg',7,0),(20,'pdf','','Ceci est un pdf','vign_real_img2.jpg','real_img2.jpg',8,0),(21,'video','http://www.youtube.com/fff','Ceci est une vidéo','vign_real_img2.jpg','real_img2.jpg',9,0),(25,'img','','legend','decoration-2349090__480.jpg','decoration-2349090__480.jpg',13,0),(26,'img','','legend','furniture-2603068__480.jpg','furniture-2603068__480.jpg',13,0),(27,'img','','legend','interior-1961070__480.jpg','interior-1961070__480.jpg',13,1),(28,'img','','legend','kitchen-2400367__480.jpg','kitchen-2400367__480.jpg',14,1),(29,'img','','legend','office-space-1744803__480.jpg','office-space-1744803__480.jpg',15,1),(30,'img','','legend','apartment-2094702__480.jpg','apartment-2094702__480.jpg',16,1),(31,'img','','legend','living-room-2155353__480.jpg','living-room-2155353__480.jpg',17,1),(32,'img','','legend','furniture-2603068__480.jpg','furniture-2603068__480.jpg',18,1),(33,'img','','legend','playroom-1246839__480.jpg','playroom-1246839__480.jpg',19,1),(34,'img','','legend','architecture-2804083__480.jpg','architecture-2804083__480.jpg',20,1),(35,'img','','legend','kitchen-1940175__480.jpg','kitchen-1940175__480.jpg',21,1),(37,'img','','legend','couv20-218x300.jpg','couv20-218x300.jpg',23,1),(38,'img','','legend','couvelle-218x300.jpg','couvelle-218x300.jpg',24,1),(39,'img','','legend','LExpress-220x300.jpg','LExpress-220x300.jpg',22,1);
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `lastLogin` datetime DEFAULT NULL,
+  PRIMARY KEY (`iduser`),
+  UNIQUE KEY `pseudo_UNIQUE` (`pseudo`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','123','alexis.ducerf@homesweethome.com',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-07 22:22:23
